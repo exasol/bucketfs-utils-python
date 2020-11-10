8d1a6dc35d28b630d3cd4e90de519255f91d14bb
Anastasiia Sergienko <anastasiia.sergienko@exasol.com>
2020-11-10 15:37:40 +0100

    #1: Adjusted an integration test

--- tests/bucketfs_utils.py
+++ tests/bucketfs_utils.py
@@ -1,8 +1,3 @@
-from pathlib import Path
-
-import requests
-
-
 class BucketFSCredentials:
     def __init__(self, host="localhost", port=6666, user="w", pwd="write"):
         self.host = host
@@ -16,33 +11,6 @@ class BucketFsConfig:
         self.credentials = credentials
         self.bucket = bucket
         self.bucketfs_name = bucketfs_name
-
-
-def upload_file_to_bucketfs(bucket_config: BucketFsConfig, file_name: str, file_path: Path):
-    with file_path.open("rb") as f:
-        url = generate_bucketfs_url(bucket_config, file_name)
-        response = requests.put(url, data=f)
-        response.raise_for_status()
-        path = get_bucketfs_path(bucket_config, file_name)
-        return url, path
-
-
-def upload_string_to_bucketfs(bucketfs_config: BucketFsConfig, file_name: str, string: str):
-    url = generate_bucketfs_url(bucketfs_config, file_name)
-    response = requests.put(url, data=string.encode("UTF-8"))
-    response.raise_for_status()
-    path = get_bucketfs_path(bucketfs_config, file_name)
-    return url, path
-
-
-def get_bucketfs_path(bucketfs_config: BucketFsConfig, file_name: str):
-    archive_extensions = [".tar.gz", ".tar.bz2", ".zip", ".tar"]
-    for extension in archive_extensions:
-        if file_name.endswith(extension):
-            file_name = file_name[:-len(extension)]
-            break
-    path = f"/buckets/{bucketfs_config.bucketfs_name}/{bucketfs_config.bucket}/{file_name}"
-    return path
 
 
 def generate_bucketfs_url(bucketfs_config: BucketFsConfig, file_name: str, with_credentials: bool = True):
--- tests/test_github_release_file_bucketfs_uploader.py
+++ tests/test_github_release_file_bucketfs_uploader.py
@@ -4,20 +4,6 @@ from extension_downloading.github_release_file_bucketfs_uploader import GithubRe
 from tests.bucketfs_utils import BucketFsConfig, generate_bucketfs_url, BucketFSCredentials
 
 
-class DatabaseCredentials:
-    def __init__(self, host="localhost", port=8888, user="sys", pwd="exasol"):
-        self.host = host
-        self.port = port
-        self.user = user
-        self.pwd = pwd
-
-
-class DatabaseConfig:
-    def __init__(self, credentials: DatabaseCredentials, schema: str):
-        self.credentials = credentials
-        self.schema = schema
-
-
 def upload_file_to_bucket(bucketfs_config):
     release_uploader = \
         GithubReleaseFileBucketFSUploader(file_to_download_name="virtual-schema-dist",
@@ -32,9 +18,8 @@ def upload_file_to_bucket(bucketfs_config):
 
 
 def test_uploading_github_release_to_bucketfs():
-    database_config = DatabaseConfig(DatabaseCredentials(), "test_schema")
     bucketfs_config = BucketFsConfig(BucketFSCredentials())
     upload_file_to_bucket(bucketfs_config)
-    response = requests.get("http://{}:{}/virtualschemas/".format(database_config.credentials.host,
-                                                                  database_config.credentials.port))
+    response = requests.get("http://{}:{}/virtualschemas/".format(bucketfs_config.credentials.host,
+                                                                  bucketfs_config.credentials.port))
     assert "virtual-schema-dist" in response.text
