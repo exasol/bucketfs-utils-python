04613a100c2f0a840da88c06c2f1f12f966ef31a
MarleneKress79789 <marlene.kress@gmx.de>
2021-11-19 14:57:43 +0100

    #13: added reading files from BucketFS to files and fileobjects

---
+++ exasol_bucketfs_utils_python/load_file_from_local_fs.py
@@ -0,0 +1,51 @@
+from exasol_bucketfs_utils_python.bucket_config import BucketConfig
+from exasol_bucketfs_utils_python.bucketfs_utils import generate_bucket_udf_path
+import typing
+from pathlib import Path
+# TODO remove class
+
+class LoadFromLocalFS:
+    """
+
+    """
+    #CONNECTION_NAME = "tensorflow_config"
+
+    def read_file_from_bucketfs_to_string(self, ctx, exa, bucket_config: BucketConfig) -> str:
+        """
+
+        """
+        bucket_file_path = ctx.path
+        if bucket_file_path is None:
+            raise ValueError("bucket_file_path can't be None")
+        bucket_path = generate_bucket_udf_path(bucket_config, bucket_file_path)
+        text_as_string = open(bucket_path).read()
+        return text_as_string
+
+    def read_file_from_bucketfs_to_file(self, ctx, exa, bucket_config: BucketConfig, local_file_path: Path):
+        """
+
+        """
+        with local_file_path.open("wb") as f:
+            self.read_file_from_bucketfs_to_fileobj(ctx, exa, bucket_config, fileobj=f)
+
+    def read_file_from_bucketfs_to_fileobj(self, ctx, exa, bucket_config: BucketConfig, fileobj: typing.IO):
+        """
+
+        """
+        bucket_file_path = ctx.path
+        if bucket_file_path is None:
+            raise ValueError("bucket_file_path can't be None")
+        bucket_path = generate_bucket_udf_path(bucket_config, bucket_file_path)
+        with open(bucket_path, "rb") as file:
+            file.seek(0)
+            fileobj.write(file.read())
+
+        #return fileobj
+
+
+    def read_file_from_bucketfs_via_joblib(self, ctx, exa, bucket_config: BucketConfig):
+        """
+
+        """
+        # to file
+        pass
--- tests/integration_tests/test_load_fs_file_from_udf.py
+++ tests/integration_tests/test_load_fs_file_from_udf.py
@@ -4,12 +4,15 @@ from pathlib import Path
 import pyexasol
 import pytest
 from tempfile import NamedTemporaryFile
+import requests
 
 from exasol_udf_mock_python.connection import Connection
                                                             #TODO something is strange with poetry install of
                                                             # exasol_udf_mock_python and dill
 
+from exasol_bucketfs_utils_python.bucketfs_utils import generate_bucket_http_url, create_auth_object
 from exasol_bucketfs_utils_python.bucketfs_factory import BucketFSFactory
+from exasol_bucketfs_utils_python.bucket_config import BucketConfig
 from exasol_bucketfs_utils_python import upload
 
 
@@ -24,7 +27,7 @@ def pyexasol_connection(db_connection):
     conn = pyexasol.connect(dsn=db_connection.address, user=db_connection.user, password=db_connection.password)
     return conn
 
-#mynote : this is needed to use language container
+
 @pytest.fixture(scope="session")
 def upload_language_container(pyexasol_connection, language_container):
     container_connection = Connection(address=f"http://localhost:6666/default/container;bfsdefault",
@@ -40,8 +43,9 @@ def upload_language_container(pyexasol_connection, language_container):
     alter_session = Path(language_container["alter_session"])
     pyexasol_connection.execute(f"ALTER SYSTEM SET SCRIPT_LANGUAGES='{alter_session}'")
     pyexasol_connection.execute(f"ALTER SESSION SET SCRIPT_LANGUAGES='{alter_session}'")
-    with open(container_path, "rb") as container_file:
+    with open(container_path, "rb") as container_file: #TODO
         container_bucketfs_location.upload_fileobj_to_bucketfs(container_file, "ml.tar")
+
 
 
 @pytest.fixture(scope="session")
@@ -58,97 +62,181 @@ def bucketfs_location():
     return container_bucketfs_location
 
 
-#@pytest.fixture(scope="session")
-#def bucket_config():
-#    bucket_name = "default"
-#    bucketfs_name = "bfsdefault"
-#    connection_config = BucketFSConnectionConfig(host="localhost",
-#                                                 port=6666,
-#                                                 user="r", pwd="read",
-#                                                 is_https=False)
-#    bucketfs_config = BucketFSConfig(bucketfs_name, connection_config=connection_config)
-#    bucket_config = BucketConfig(bucket_name, bucketfs_config)
-
-
-# TODO write  actual tests
 # TODO test for missing file? test for wrong file format, invalid bucket obj, error while writing?
 
+def delete_testfile_from_BucketFS(bucket_config: BucketConfig, file_path: str):
+    url = generate_bucket_http_url(bucket_config, file_path)
+    auth = create_auth_object(bucket_config)
+    response = requests.delete(url.geturl(), auth=auth)
+    response.raise_for_status()
 
-def test_load_file_to_string(upload_language_container, pyexasol_connection, bucketfs_location, bucket_config):
-    test_input_string = "test_byte_string"
-    # upload file
+def upload_testfile_to_BucketFS(bucket_config: BucketConfig, file_path: str, content: str):
     with NamedTemporaryFile() as input_temp_file:
-        input_temp_file.write(test_input_string.encode('utf-8'))
+        input_temp_file.write(content.encode('utf-8'))
         input_temp_file.flush()
 
-        path_in_bucket = "path/in/bucket/file.txt"
         upload.upload_file_to_bucketfs(
-            bucket_config=bucketfs_location.bucket_config,
-            bucket_file_path=path_in_bucket,
+            bucket_config=bucket_config,
+            bucket_file_path=file_path,
             local_file_path=Path(input_temp_file.name))
-        try:
 
-            # load file from udf
-            target_schema = "TARGET_SCHEMA"
-            pyexasol_connection.execute(f"CREATE SCHEMA IF NOT EXISTS {target_schema};") #mynotes dropfirst?
-            pyexasol_connection.execute(f"OPEN SCHEMA {target_schema};")
-            udf_sql = textwrap.dedent(f"""
-                CREATE OR REPLACE PYTHON3_BFSUP SET SCRIPT {target_schema}."LoadFsFileFrom_UDF"(
-                      "bucket_config" VARCHAR(2000000),
-                      "path" VARCHAR(20000)
-                )
-                RETURNS VARCHAR(20000)
-                AS 
-                from exasol_bucketfs_utils_python.bucket_config import BucketConfig
-                from exasol_bucketfs_utils_python.bucketfs_connection_config import BucketFSConnectionConfig
-                from exasol_bucketfs_utils_python.bucketfs_config import BucketFSConfig
-                def get_bucket_config():
-                    bucket_name = "default"
-                    bucketfs_name = "bfsdefault"
-                    connection_config = BucketFSConnectionConfig(host="localhost",
-                                                                 port=6666,
-                                                                 user="r", pwd="read",
-                                                                 is_https=False)
-                    bucketfs_config = BucketFSConfig(bucketfs_name, connection_config=connection_config)
-                    return BucketConfig(bucket_name, bucketfs_config)
+
+def test_load_file_to_string(upload_language_container, pyexasol_connection, bucketfs_location):
+    test_input_string = "test_byte_string"
+    path_in_bucket = "path/in/bucket/file.txt"
+    # upload file
+    upload_testfile_to_BucketFS(bucketfs_location.bucket_config, path_in_bucket, test_input_string)
+    try:
+
+        # load file from udf
+        target_schema = "TARGET_SCHEMA"
+        pyexasol_connection.execute(f"CREATE SCHEMA IF NOT EXISTS {target_schema};") #mynotes dropfirst?
+        pyexasol_connection.execute(f"OPEN SCHEMA {target_schema};")
+        udf_sql = textwrap.dedent(f"""
+            CREATE OR REPLACE PYTHON3_BFSUP SET SCRIPT {target_schema}."LoadFromLocalFS"(
+                  "bucket_config" VARCHAR(2000000),
+                  "path" VARCHAR(20000)
+            )
+            RETURNS VARCHAR(20000)
+            AS 
+            from exasol_bucketfs_utils_python.bucket_config import BucketConfig
+            from exasol_bucketfs_utils_python.bucketfs_connection_config import BucketFSConnectionConfig
+            from exasol_bucketfs_utils_python.bucketfs_config import BucketFSConfig
+            def get_bucket_config():
+                bucket_name = "default"
+                bucketfs_name = "bfsdefault"
+                connection_config = BucketFSConnectionConfig(host="localhost",
+                                                             port=6666,
+                                                             user="r", pwd="read",
+                                                             is_https=False)
+                bucketfs_config = BucketFSConfig(bucketfs_name, connection_config=connection_config)
+                return BucketConfig(bucket_name, bucketfs_config)
                 
-                def run(ctx):
-                    bucket_config = get_bucket_config()
-                    from exasol_bucketfs_utils_python.load_fs_file_from_udf import LoadFsFileFromUDF
-                    udf = LoadFsFileFromUDF()
-                    x = udf.run(ctx, exa, bucket_config)
-                    return x
-                """)
-            pyexasol_connection.execute(udf_sql)
-            #TODO how get bucket config in there? or is this ok?
-            output_test_string = pyexasol_connection.execute(
-                f"""select {target_schema}."LoadFsFileFrom_UDF"('{bucketfs_location.bucket_config}','{path_in_bucket}'
-                )""").fetchall()
-            assert output_test_string == test_input_string
-    #TODO delete file?
-        finally:
-            pyexasol_connection.execute(f"DROP SCHEMA IF EXISTS {target_schema} CASCADE;")
+            def run(ctx):
+                bucket_config = get_bucket_config()
+                from exasol_bucketfs_utils_python.load_file_from_local_fs import LoadFromLocalFS
+                udf = LoadFromLocalFS()
+                output_test_string = udf.read_file_from_bucketfs_to_string(ctx, exa, bucket_config)
+                return output_test_string
+            """)
+        pyexasol_connection.execute(udf_sql)
+        output_test_string = pyexasol_connection.execute(
+            f"""select {target_schema}."LoadFromLocalFS"('{bucketfs_location.bucket_config}','{path_in_bucket}'
+            )""").fetchall()
+        assert output_test_string[0][0] == test_input_string
+    finally:
+        delete_testfile_from_BucketFS(file_path=path_in_bucket,
+                                      bucket_config=bucketfs_location.bucket_config)
+        pyexasol_connection.execute(f"DROP SCHEMA IF EXISTS {target_schema} CASCADE;")
 
 
-def test_load_file_to_fileObj(pyexasol_connection):
-    bucket_object = ""
-    file_path = ""
-    file = ""
+def test_load_file_to_fileObj(upload_language_container, pyexasol_connection, bucketfs_location):
+    test_input_string = "test_byte_string"
+    path_in_bucket = "path/in/bucket/file.txt"
     # upload file
-    # load file from udf
-    # assert loaded file = orig file
-    # cleanup
+    upload_testfile_to_BucketFS(bucketfs_location.bucket_config, path_in_bucket, test_input_string)
+    try:
 
-def test_load_file_to_file(pyexasol_connection):
-    bucket_object = ""
-    file_path = ""
-    file = ""
+        # load file from udf
+        target_schema = "TARGET_SCHEMA"
+        pyexasol_connection.execute(f"CREATE SCHEMA IF NOT EXISTS {target_schema};")  # mynotes dropfirst?
+        pyexasol_connection.execute(f"OPEN SCHEMA {target_schema};")
+        udf_sql = textwrap.dedent(f"""
+            CREATE OR REPLACE PYTHON3_BFSUP SET SCRIPT {target_schema}."LoadFromLocalFS"(
+                "bucket_config" VARCHAR(2000000),
+                "path" VARCHAR(20000)
+            )
+            RETURNS VARCHAR(20000)
+            AS 
+            from exasol_bucketfs_utils_python.bucket_config import BucketConfig
+            from exasol_bucketfs_utils_python.bucketfs_connection_config import BucketFSConnectionConfig
+            from exasol_bucketfs_utils_python.bucketfs_config import BucketFSConfig
+            from tempfile import NamedTemporaryFile
+            def get_bucket_config():
+                bucket_name = "default"
+                bucketfs_name = "bfsdefault"
+                connection_config = BucketFSConnectionConfig(host="localhost",
+                                                             port=6666,
+                                                             user="r", pwd="read",
+                                                             is_https=False)
+                bucketfs_config = BucketFSConfig(bucketfs_name, connection_config=connection_config)
+                return BucketConfig(bucket_name, bucketfs_config)
+
+            def run(ctx):
+                bucket_config = get_bucket_config()
+                from exasol_bucketfs_utils_python.load_file_from_local_fs import LoadFromLocalFS
+                udf = LoadFromLocalFS()
+                with NamedTemporaryFile() as output_temp_file:
+                    udf.read_file_from_bucketfs_to_fileobj(ctx, exa, bucket_config, output_temp_file)
+                    output_temp_file.flush()
+                    output_temp_file.seek(0)
+                    output_file_content = output_temp_file.read()
+                return output_file_content
+            """)
+        pyexasol_connection.execute(udf_sql)
+        output_test_string = pyexasol_connection.execute(
+            f"""select {target_schema}."LoadFromLocalFS"('{bucketfs_location.bucket_config}','{path_in_bucket}'
+               )""").fetchall()
+        assert output_test_string[0][0] == test_input_string
+    finally:
+        delete_testfile_from_BucketFS(file_path=path_in_bucket,
+                                      bucket_config=bucketfs_location.bucket_config)
+        pyexasol_connection.execute(f"DROP SCHEMA IF EXISTS {target_schema} CASCADE;")
+
+def test_load_file_to_file(upload_language_container, pyexasol_connection, bucketfs_location):
+    test_input_string = "test_byte_string"
+    path_in_bucket = "path/in/bucket/file.txt"
     # upload file
-    # load file from udf
-    # assert loaded file = orig file
-    # cleanup
+    upload_testfile_to_BucketFS(bucketfs_location.bucket_config, path_in_bucket, test_input_string)
+    try:
 
-def test_load_file_to_object_via_joblib(pyexasol_connection):
+        # load file from udf
+        target_schema = "TARGET_SCHEMA"
+        pyexasol_connection.execute(f"CREATE SCHEMA IF NOT EXISTS {target_schema};")  # mynotes dropfirst?
+        pyexasol_connection.execute(f"OPEN SCHEMA {target_schema};")
+        udf_sql = textwrap.dedent(f"""
+            CREATE OR REPLACE PYTHON3_BFSUP SET SCRIPT {target_schema}."LoadFromLocalFS"(
+                "bucket_config" VARCHAR(2000000),
+                "path" VARCHAR(20000)
+            )
+            RETURNS VARCHAR(20000)
+            AS 
+            from exasol_bucketfs_utils_python.bucket_config import BucketConfig
+            from exasol_bucketfs_utils_python.bucketfs_connection_config import BucketFSConnectionConfig
+            from exasol_bucketfs_utils_python.bucketfs_config import BucketFSConfig
+            from tempfile import NamedTemporaryFile
+            from pathlib import Path
+            def get_bucket_config():
+                bucket_name = "default"
+                bucketfs_name = "bfsdefault"
+                connection_config = BucketFSConnectionConfig(host="localhost",
+                                                             port=6666,
+                                                             user="r", pwd="read",
+                                                             is_https=False)
+                bucketfs_config = BucketFSConfig(bucketfs_name, connection_config=connection_config)
+                return BucketConfig(bucket_name, bucketfs_config)
+
+            def run(ctx):
+                bucket_config = get_bucket_config()
+                from exasol_bucketfs_utils_python.load_file_from_local_fs import LoadFromLocalFS
+                udf = LoadFromLocalFS()
+                with NamedTemporaryFile() as output_temp_file:
+                    udf.read_file_from_bucketfs_to_file(ctx, exa, bucket_config, Path(output_temp_file.name))
+                    output_test_byte_string = output_temp_file.read()
+                return output_test_byte_string
+            """)
+        pyexasol_connection.execute(udf_sql)
+        output_test_string = pyexasol_connection.execute(
+            f"""select {target_schema}."LoadFromLocalFS"('{bucketfs_location.bucket_config}','{path_in_bucket}'
+               )""").fetchall()
+        assert output_test_string[0][0] == test_input_string.encode('utf-8')
+    finally:
+        delete_testfile_from_BucketFS(file_path=path_in_bucket,
+                                      bucket_config=bucketfs_location.bucket_config)
+        pyexasol_connection.execute(f"DROP SCHEMA IF EXISTS {target_schema} CASCADE;")
+
+
+def test_load_file_to_object_via_joblib(upload_language_container, pyexasol_connection, bucketfs_location):
     bucket_object = ""
     file_path = ""
     file = ""
