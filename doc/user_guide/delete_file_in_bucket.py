f89ddd8b5745ff909c27912bc5fe2cc2072b0ab1
“umitbuyuksahin” <ucbuyuksahin@gmail.com>
2022-04-29 11:41:27 +0200

    Added user guide for deletion

---
+++ doc/user_guide/delete_file_in_bucket.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from exasol_bucketfs_utils_python import upload, delete
+from exasol_bucketfs_utils_python.bucket_config import BucketConfig
+from exasol_bucketfs_utils_python.bucketfs_config import BucketFSConfig
+from exasol_bucketfs_utils_python.bucketfs_connection_config import BucketFSConnectionConfig
+
+connection_config = BucketFSConnectionConfig(
+    host="localhost", port=6666,
+    user="w", pwd="write",
+    is_https=False)
+bucketfs_config = BucketFSConfig(
+    connection_config=connection_config,
+    bucketfs_name="bfsdefault")
+bucket_config = BucketConfig(
+    bucket_name="default",
+    bucketfs_config=bucketfs_config)
+
+local_input_file_path = Path("local_input_file.txt")
+path_in_bucket = "path/in/bucket/file.txt"
+upload.upload_file_to_bucketfs(
+    bucket_config=bucket_config,
+    bucket_file_path=path_in_bucket,
+    local_file_path=local_input_file_path)
+
+delete.delete_file_in_bucketfs(
+    bucket_config=bucket_config,
+    bucket_file_path=path_in_bucket)
