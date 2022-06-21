24721f5cc1a17a261b9d65ee9564145d8b41d560
Torsten Kilias <torsten.kilias@exasol.com>
2022-06-21 15:21:52 +0200

    Add type hint for nox session to noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -2,31 +2,31 @@ import nox
 
 
 @nox.session
-def build_html_doc(session):
+def build_html_doc(session: nox.Session):
     BASE_PATH = _get_base_path(session)
     with session.chdir(BASE_PATH[:-1] + "/doc"):
         _build_html_doc(session)
 
 
-def _get_base_path(session):
+def _get_base_path(session: nox.Session):
     BASE_PATH = session.run("git", "rev-parse", "--show-toplevel", silent=True)
     return BASE_PATH
 
 
-def _build_html_doc(session):
+def _build_html_doc(session: nox.Session):
     session.run("sphinx-apidoc", "-T", "-e", "-o", "api", "../exasol_bucketfs_utils_python")
     session.run("sphinx-build", "-b", "html", "-W", ".", ".build-docu")
 
 
 @nox.session
-def open_html_doc(session):
+def open_html_doc(session: nox.Session):
     BASE_PATH = _get_base_path(session)
     with session.chdir(BASE_PATH[:-1] + "/doc"):
         session.run("xdg-open", ".build-docu/index.html")
 
 
 @nox.session
-def build_and_open_html_doc(session):
+def build_and_open_html_doc(session: nox.Session):
     BASE_PATH = _get_base_path(session)
     with session.chdir(BASE_PATH[:-1] + "/doc"):
         _build_html_doc(session)
@@ -34,7 +34,7 @@ def build_and_open_html_doc(session):
 
 
 @nox.session
-def commit_pages_main(session):
+def commit_pages_main(session: nox.Session):
     BASE_PATH = _get_base_path(session)
     with session.chdir(BASE_PATH[:-1]):
         session.run("sgpg",
@@ -43,11 +43,11 @@ def commit_pages_main(session):
                     "--push_enabled", "commit",
                     "--source_branch", "main",
                     "--module_path", "${StringArray[@]}",
-                    env={"StringArray":("../exasol-bucketfs-utils-python")})
+                    env={"StringArray": ("../exasol-bucketfs-utils-python")})
 
 
 @nox.session
-def commit_pages_current(session):
+def commit_pages_current(session: nox.Session):
     BASE_PATH = _get_base_path(session)
     branch = session.run("git", "branch", "--show-current", silent=True)
     with session.chdir(BASE_PATH[:-1]):
@@ -60,7 +60,7 @@ def commit_pages_current(session):
 
 
 @nox.session
-def push_pages_main(session):
+def push_pages_main(session: nox.Session):
     BASE_PATH = _get_base_path(session)
     with session.chdir(BASE_PATH[:-1]):
         session.run("sgpg",
@@ -73,7 +73,7 @@ def push_pages_main(session):
 
 
 @nox.session
-def push_pages_current(session):
+def push_pages_current(session: nox.Session):
     BASE_PATH = _get_base_path(session)
     branch = session.run("git", "branch", "--show-current", silent=True)
     with session.chdir(BASE_PATH[:-1]):
@@ -86,7 +86,7 @@ def push_pages_current(session):
 
 
 @nox.session
-def push_pages_release(session):
+def push_pages_release(session: nox.Session):
     BASE_PATH = _get_base_path(session)
     tags = session.run("git", "tag", "--sort=committerdate", silent=True)
     # get the latest tag. last element in list is empty string, so choose second to last
@@ -96,14 +96,14 @@ def push_pages_release(session):
                     "--target_branch", "github-pages/main",
                     "--push_origin", "origin",
                     "--push_enabled", "push",
-                    "--source_branch",  tag,
+                    "--source_branch", tag,
                     "--source_origin", "tags",
                     "--module_path", "${StringArray[@]}",
                     env={"StringArray": ("../exasol-bucketfs-utils-python")})
 
 
 @nox.session
-def run_tests(session):
+def run_tests(session: nox.Session):
     BASE_PATH = _get_base_path(session)
     with session.chdir(BASE_PATH[:-1]):
         session.run("pytest", "tests")
