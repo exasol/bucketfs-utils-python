637d00d5a5714cd212f9a381b05bb6fba615528d
Torsten Kilias <torsten.kilias@exasol.com>
2022-06-22 11:37:26 +0200

    Add name and docstring to nox session. Set python to none for sessions.

--- noxfile.py
+++ noxfile.py
@@ -21,30 +21,37 @@ def _open_docs_in_browser(session: nox.Session):
     webbrowser.open_new_tab(index_file_path.as_uri())
 
 
-@nox.session
+@nox.session(name="build-html-doc", python=None)
 def build_html_doc(session: nox.Session):
+    """Build the documentation for current checkout"""
     base_path = _get_base_path(session)
     with session.chdir(base_path[:-1] + "/doc"):
         _build_html_doc(session)
 
 
-@nox.session
+@nox.session(name="open-html-doc", python=None)
 def open_html_doc(session: nox.Session):
+    """Open the documentation for current checkout in the browser"""
     base_path = _get_base_path(session)
     with session.chdir(base_path[:-1] + "/doc"):
         _open_docs_in_browser(session)
 
 
-@nox.session
+@nox.session(name="build-and-open-html-doc", python=None)
 def build_and_open_html_doc(session: nox.Session):
+    """Build and open the documentation for current checkout in browser"""
     base_path = _get_base_path(session)
     with session.chdir(base_path[:-1] + "/doc"):
         _build_html_doc(session)
         _open_docs_in_browser(session)
 
 
-@nox.session
+@nox.session(name="commit-pages-main", python=None)
 def commit_pages_main(session: nox.Session):
+    """
+    Generate the GitHub pages documentation for the main branch and
+    commit it to the branch github-pages/main
+    """
     base_path = _get_base_path(session)
     with session.chdir(base_path[:-1]):
         session.run("sgpg",
@@ -56,8 +63,12 @@ def commit_pages_main(session: nox.Session):
                     env={"StringArray": ("../exasol-bucketfs-utils-python")})
 
 
-@nox.session
+@nox.session(name="commit-pages-current", python=None)
 def commit_pages_current(session: nox.Session):
+    """
+    Generate the GitHub pages documentation for the current branch and
+    commit it to the branch github-pages/<current_branch>
+    """
     base_path = _get_base_path(session)
     branch = session.run("git", "branch", "--show-current", silent=True)
     with session.chdir(base_path[:-1]):
@@ -69,8 +80,12 @@ def commit_pages_current(session: nox.Session):
                     env={"StringArray": ("../exasol-bucketfs-utils-python")})
 
 
-@nox.session
+@nox.session(name="push-pages-main", python=None)
 def push_pages_main(session: nox.Session):
+    """
+    Generate the GitHub pages documentation for the main branch and
+    pushes it to the remote branch github-pages/main
+    """
     base_path = _get_base_path(session)
     with session.chdir(base_path[:-1]):
         session.run("sgpg",
@@ -82,8 +97,12 @@ def push_pages_main(session: nox.Session):
                     env={"StringArray": ("../exasol-bucketfs-utils-python")})
 
 
-@nox.session
+@nox.session(name="push-pages-current", python=None)
 def push_pages_current(session: nox.Session):
+    """
+    Generate the GitHub pages documentation for the current branch and
+    pushes it to the remote branch github-pages/<current_branch>
+    """
     base_path = _get_base_path(session)
     branch = session.run("git", "branch", "--show-current", silent=True)
     with session.chdir(base_path[:-1]):
@@ -95,8 +114,9 @@ def push_pages_current(session: nox.Session):
                     env={"StringArray": ("../exasol-bucketfs-utils-python")})
 
 
-@nox.session
+@nox.session(name="push-pages-release", python=None)
 def push_pages_release(session: nox.Session):
+    """Generate the GitHub pages documentation for the release and pushes it to the remote branch github-pages/main"""
     base_path = _get_base_path(session)
     tags = session.run("git", "tag", "--sort=committerdate", silent=True)
     # get the latest tag. last element in list is empty string, so choose second to last
@@ -112,8 +132,9 @@ def push_pages_release(session: nox.Session):
                     env={"StringArray": ("../exasol-bucketfs-utils-python")})
 
 
-@nox.session
+@nox.session(name="run-tests", python=None)
 def run_tests(session: nox.Session):
+    """Run the tests in the poetry environment"""
     base_path = _get_base_path(session)
     with session.chdir(base_path[:-1]):
         session.run("pytest", "tests")
